cf4e49eab5643f7caebca89039310f1e7aab967d
Remco Zuidam <r.zuidam@mendrix.nl>
2020-05-12 17:00:59 +0200

    - fixed: working directory was not set correctly - updated: example

--- example/install.js
+++ example/install.js
@@ -2,41 +2,42 @@ var Service = require('../').Service;
 
 // Create a new service object
 var svc = new Service({
-  name:'Hello World',
-  id: 'SomeThingCompletelyDifferent',
-  description: 'The nodejs.org example web server.',
-  script: require('path').join(__dirname, 'helloworld.js'),
-  env:{
-    name: "NODE_ENV",
-    value: "production"
-  },
-  resetFailure: "10 secs",
-  onFailure: [
-      {
-        action: "restart",
-        delay: "10 secs"
-      },
-      {
-        action: "restart",
-        delay: "20 secs"
-      }]
+    name: 'Hello World',
+    id: 'SomeThingCompletelyDifferent',
+    description: 'The nodejs.org example web server.',
+    script: require('path').join(__dirname, 'helloworld.js'),
+    env: {
+        name: "NODE_ENV",
+        value: "production"
+    },
+    resetFailure: "10 secs",
+    onFailure: [
+        {
+            action: "restart",
+            delay: "10 secs"
+        },
+        {
+            action: "restart",
+            delay: "20 secs"
+        }],
+    workingDirectory: "C:\\temp"
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
-  svc.start();
+svc.on('install', function () {
+    svc.start();
 });
 
 // Just in case this file is run twice.
-svc.on('alreadyinstalled',function(){
-  console.log('This service is already installed.');
+svc.on('alreadyinstalled', function () {
+    console.log('This service is already installed.');
 });
 
 // Listen for the "start" event and let us know when the
 // process has actually started working.
-svc.on('start',function(){
-  console.log(svc.name+' started!\nVisit http://127.0.0.1:3000 to see it in action.');
+svc.on('start', function () {
+    console.log(svc.name + ' started!\nVisit http://127.0.0.1:3000 to see it in action.');
 });
 
 // Install the script as a service.
